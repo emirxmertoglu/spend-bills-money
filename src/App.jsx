9c72368e7dc2470ff358257014126c9233a37596
emirxmertoglu <emir.mertoglu.97@gmail.com>
2023-06-03 14:09:21 +0300

    some UI improvements

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,12 @@ import MoneyBar from "@/components/MoneyBar";
 
 const App = () => {
   return (
-    <div className="container mx-auto py-8">
-      <div className="flex flex-col gap-4">
-        <Header />
-        <MoneyBar />
+    <div className="p-4 lg:p-8 h-full bg-gray-100">
+      <div className="container max-w-[1000px] mx-auto">
+        <div className="flex flex-col gap-4">
+          <Header />
+          <MoneyBar />
+        </div>
       </div>
     </div>
   );
--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,13 +1,15 @@
 const Header = () => {
   return (
-    <div className="flex flex-col items-center justify-center gap-4">
+    <div className="py-12 px-4 flex flex-col items-center justify-center gap-4 bg-white rounded-sm">
       <img
-        className="w-24 h-24 rounded-full object-contain"
+        className="w-32 h-32 rounded-full object-contain"
         src="https://imageio.forbes.com/specials-images/imageserve/62d599ede3ff49f348f9b9b4/0x0.jpg?format=jpg&crop=821,821,x155,y340,safe&height=416&width=416&fit=bounds"
         alt="Bill Gates"
       />
 
-      <div className="text-3xl font-bold">Spend Bill Gates' Money</div>
+      <div className="text-3xl font-bold text-center">
+        Spend Bill Gates' Money
+      </div>
     </div>
   );
 };
